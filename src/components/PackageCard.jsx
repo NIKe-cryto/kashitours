01870964d903e1a4df7364fa49101cd0b620f70f
NIKe-cryto <nikhil2812005@gmail.com>
2026-04-27 06:13:13 +0530

    Add PackageCard component

---
+++ src/components/PackageCard.jsx
@@ -0,0 +1,13 @@
+// PackageCard.jsx
+import React from 'react';
+
+const PackageCard = () => {
+    return (
+        <div className="package-card">
+            <h2>Package Title</h2>
+            <p>Description of the package.</p>
+        </div>
+    );
+};
+
+export default PackageCard;
